704c8a6d7516d18353dbc90331a7f42107dbbe57
Lance Hepler <lance.hepler@10xgenomics.com>
2019-12-18 13:33:12 -0800

    fix whitespace issues cause by lack of vim config

--- InOutStreams.h
+++ InOutStreams.h
@@ -8,21 +8,20 @@
 
 template<class charT, class traits = std::char_traits<charT> >
 class NullBuf: public std::basic_streambuf<charT, traits> {
-    inline
-    typename traits::int_type overflow(typename traits::int_type c) {
-	return traits::not_eof(c);
+    inline typename traits::int_type overflow(typename traits::int_type c) {
+        return traits::not_eof(c);
     }
 };
 
 template<class charT = char, class traits = std::char_traits<charT> >
 class NullStream: public std::basic_ostream<charT, traits> {
     public:
-	inline NullStream():
-	    std::basic_ios<charT, traits>(&nullbuf),
+        inline NullStream():
+            std::basic_ios<charT, traits>(&nullbuf),
             std::basic_ostream<charT, traits>(&nullbuf)
         { std::basic_ios<charT, traits>::init(&nullbuf); }
-	inline void open(const char*, std::ios_base::openmode = std::ios_base::out) {}
-	inline void close() {}
+        inline void open(const char*, std::ios_base::openmode = std::ios_base::out) {}
+        inline void close() {}
 
     private:
         NullBuf<charT, traits> nullbuf;
