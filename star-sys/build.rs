cc5fb9ce341fd78989e819a34153705e06cf9a9d
Lance Hepler <lance.hepler@10xgenomics.com>
2020-07-13 15:53:12 -0700

    also rerun build if headers change

--- star-sys/build.rs
+++ star-sys/build.rs
@@ -62,9 +62,60 @@ const FILES: &[&str] = &[
     "STAR/source/ErrorWarning.cpp",
 ];
 
+// I used the following monstrosity to generate this list:
+// head -62 ../build.rs | tail -40 | sed -e 's|\s\+"STAR\/||' | sed -e 's|",||' | xargs -I{} grep "#include" {} | sort | uniq | grep -v "<*>" | grep -v "^\/\/" | sed -e 's|^\s\+||' | sed -e 's|SAMTOOLS_BGZF_H|"htslib/htslib/bgzf.h"|' | sed -e 's|#include\ "|"STAR\/source\/|' | sed -e 's|"$|",|' | sort >> ../build.rs
+const HEADERS: &[&str] = &[
+    "STAR/source/alignSmithWaterman.h",
+    "STAR/source/binarySearch2.h",
+    "STAR/source/blocksOverlap.h",
+    "STAR/source/ErrorWarning.h",
+    "STAR/source/extendAlign.h",
+    "STAR/source/funCompareUintAndSuffixes.h",
+    "STAR/source/funCompareUintAndSuffixesMemcmp.h",
+    "STAR/source/Genome.h",
+    "STAR/source/genomeParametersWrite.h",
+    "STAR/source/genomeSAindex.h",
+    "STAR/source/genomeScanFastaFiles.h",
+    "STAR/source/GlobalVariables.h",
+    "STAR/source/htslib/htslib/bgzf.h",
+    "STAR/source/IncludeDefine.h",
+    "STAR/source/InOutStreams.h",
+    "STAR/source/insertSeqSA.h",
+    "STAR/source/loadGTF.h",
+    "STAR/source/orbit.h",
+    "STAR/source/OutSJ.h",
+    "STAR/source/PackedArray.h",
+    "STAR/source/ParametersChimeric.h",
+    "STAR/source/parametersDefault.xxd",
+    "STAR/source/Parameters.h",
+    "STAR/source/ParametersSolo.h",
+    "STAR/source/ReadAlign.h",
+    "STAR/source/readLoad.h",
+    "STAR/source/SequenceFuns.h",
+    "STAR/source/serviceFuns.cpp",
+    "STAR/source/sjAlignSplit.cpp",
+    "STAR/source/SjdbClass.h",
+    "STAR/source/sjdbInsertJunctions.h",
+    "STAR/source/sjdbLoadFromFiles.h",
+    "STAR/source/sjdbPrepare.h",
+    "STAR/source/sortSuffixesBucket.h",
+    "STAR/source/Stats.h",
+    "STAR/source/stitchWindowAligns.h",
+    "STAR/source/streamFuns.h",
+    "STAR/source/stringSubstituteAll.h",
+    "STAR/source/SuffixArrayFuns.h",
+    "STAR/source/sysRemoveDir.h",
+    "STAR/source/TimeFunctions.h",
+    "STAR/source/Transcript.h",
+    "STAR/source/Transcriptome.h",
+];
+
 fn main() {
     // do not rebuild constitutively
     for file in FILES {
+        println!("cargo:rerun-if-changed={}", file);
+    }
+    for file in HEADERS {
         println!("cargo:rerun-if-changed={}", file);
     }
     cc::Build::new()
