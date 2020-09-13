e0f189dd37374789a618cf6cd8534a383b855e02
Patrick Marks <pmarks@gmail.com>
2020-09-13 10:47:36 -0700

    drop travis, make GH actions happy. Small cleanups to error handling

--- star-sys/STAR/source/MmapArray.cpp
+++ star-sys/STAR/source/MmapArray.cpp
@@ -38,17 +38,17 @@ int MmapArray::makeMmap(string filename, size_t length, size_t suffix_padding) {
     void* addr1 = mmap(NULL, mmap_length, PROT_READ|PROT_WRITE, MAP_ANONYMOUS|MAP_PRIVATE|MAP_NORESERVE, -1, 0);
 
     if (addr1 == (void*) -1) {
-        std::cerr << "fd: " << fd;
-        std::cerr << "len pre: " << length << "len_round: " << file_mmap_length << "\n";
-        std::cerr << "addr1: " << addr1;
+        std::cerr << "reference mmap failed: fd: " << fd;
+        std::cerr << " len pre: " << length << " len_round: " << file_mmap_length << "\n";
+        std::cerr << " addr1: " << addr1 << "\n";
         return -1;
     }
 
     // map the file starting after the first page
     void* addr2 = mmap((void*) ((char*)addr1 + PAGE), file_mmap_length, PROT_READ, MAP_FIXED|MAP_PRIVATE, fd, 0);
     
-    if (addr2 == (void*) -1) {
-        std::cerr << "addr2: " << addr2;
+    if (addr2 == (void*) -1 || (char*)addr2 != (char*)addr1 + PAGE) {
+        std::cerr << "reference mmap failed: addr2: " << addr2 << "\n";
         return -1;
     }
 
@@ -61,19 +61,16 @@ int MmapArray::makeMmap(string filename, size_t length, size_t suffix_padding) {
     // make the final page writeable. On Mac at least, it's much faster if the 'main' mmap above is read only.
     void* addr3 = mmap((void*) ((char*)addr1 + file_mmap_length), PAGE, PROT_READ|PROT_WRITE, MAP_FIXED|MAP_PRIVATE, fd, 0);
     
-    if (addr3 == (void*) -1) {
-        std::cerr << "addr3: " << addr3;
+    if (addr3 == (void*) -1 || (char*)addr3 != (char*)addr1 + file_mmap_length) {
+        std::cerr << "reference mmap failed: addr3: " << addr3 << "\n";
         return -1;
     }
-
-    assert((char*)addr3 == (char*)addr1 + file_mmap_length);
-
 
     // madivse to let the kernel know we want the whole file in memory
     int res = posix_madvise((char*)addr2, file_mmap_length, POSIX_MADV_WILLNEED);
     
     if (res) {
-        std::cerr << "res: " << res;
+        std::cerr << "madivse on reference mmap failred: res: " << res << "\n";
         return -1;
     }
 
